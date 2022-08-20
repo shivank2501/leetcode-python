5dac4cd9099b92bb3b90249aa7e29e2a5096ed00
Shivank <agarwalshivank25@gmail.com>
2022-08-20 16:12:11 +0200

    vaild parentheses

---
+++ vaild_parentheses.py
@@ -0,0 +1,15 @@
+def isValid(s):
+  stack = []
+  lookup = {'}':'{',')':'(',']':'['}
+  for i in s:
+      if i in lookup.values():
+          stack.append(i)
+      elif stack and lookup[i] == stack[-1]:
+          stack.pop()
+      else:
+          return False
+  return stack == []            
+
+
+s = '{()}'
+print(isValid(s))
